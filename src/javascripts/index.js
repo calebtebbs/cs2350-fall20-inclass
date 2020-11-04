e9c030cffec94b0208c5a36ef9a15354adef919e
calebtebbs <tebbscaleb@gmail.com>
2020-11-03 20:28:48 -0700

    Final Submission

--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -5,3 +5,26 @@ require.context('../stylesheets/', true, /\.(css|scss)$/i)
 
 // JavaScript
 //TODO
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function render()
+{
+    let c = document.querySelector('canvas')
+    if(c.getContext)
+    {
+        let ctx = c.getContext('2d')
+        ctx.clearRect(0,0, 500, 300)
+        ctx.fillStyle = document.getElementById('c').value
+        ctx.fillRect(50, 100, 60, 30)
+
+        ctx.fillStyle = "orange" 
+        ctx.beginPath()
+        ctx.arc(document.getElementById('x').value, 250, 150, 75, 0, 2 * Math.PI)
+        ctx.fill()
+    }
+}
+
+document.body.onload = render
+document.getElementById('x').onchange = render
+document.getElementById('c').onchange = render
